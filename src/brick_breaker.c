fd2f9f532a8f52dfb6e36ef6fc4be7d8c613247f
mlb406 <mlb406@gmail.com>
2015-11-27 17:26:55 +0000

    Initial commit (template)
    This is the first commit. It is just a template really.

---
+++ src/brick_breaker.c
@@ -0,0 +1,25 @@
+#include <pebble.h>
+#include "brick_breaker.h"
+
+static void main_window_load() {
+  
+}
+
+static void main_window_unload() {
+  
+}
+
+static void init() {
+  
+}
+
+static void deinit() {
+  
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+  return 0;
+}
